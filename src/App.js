ec4fd648bb1e7d615510889b04f9e5151b3440a0
cuevacelis <cuevacelis@hotmail.com>
2022-01-22 20:25:19 -0500

    add login page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-
-import NavBar from './components/NavBar';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Index from './pages/index';
-import Artists from './pages/artists';
-import Pictures from './pages/pictures';
-import AboutUs from './pages/about-us';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar";
+import AboutUs from "./pages/about-us";
+import Artists from "./pages/artists";
+import LoginPage from "./pages/auth/login";
+import Index from "./pages/index";
+import Pictures from "./pages/pictures";
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/home' element={<Index />} />
-        <Route path='/artists' element={<Artists />} />
-        <Route path='/pictures' element={<Pictures />} />
-        <Route path='/about-us' element={<AboutUs />} />
-
+        <Route path="/home" element={<Index />} />
+        <Route path="/artists" element={<Artists />} />
+        <Route path="/pictures" element={<Pictures />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/auth/login.jsx
@@ -0,0 +1,15 @@
+function LoginPage(params) {
+  return (
+    <section>
+      <h1>Login</h1>
+      <form>
+        <input></input>
+        <input></input>
+        <input type={"submit"} value={"Ingresar a la sesión"} />
+      </form>
+      <div>Login de OnlyArts</div>
+    </section>
+  );
+}
+
+export default LoginPage;
